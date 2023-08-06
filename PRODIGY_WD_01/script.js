897b48374ab0bae47c0151f3b7963f16c022eafa
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-06 15:47:42 +0530

    #

--- PRODIGY_WD_01/script.js
+++ PRODIGY_WD_01/script.js
@@ -1,19 +1,51 @@
-var start = document.getElementById("start").addEventListener("click",startBtn);
-var lap = document.getElementById("lap").addEventListener("click",lapBtn);
-var reset = document.getElementById("reset").addEventListener("click",resetBtn);
+const start = document.querySelector(".start");
+start.addEventListener("click",startBtn);
+const lap = document.querySelector(".lap").addEventListener("click",lapBtn);
+const reset = document.querySelector(".reset").addEventListener("click",resetBtn);
+const lapList = document.querySelector(".ol"); 
 
-var min ,sec ,msec;
-min = sec = msec = 0;
+var min ,sec ,ms, startTimer;
+min = sec = ms = 0+"0" ;
+var numberOfLap = 1;
 
 function startBtn(){
-    start.
     console.log("start");
-}
+    start.classList.add("active");
+    
+    startTimer = setInterval(()=>{
+        ms++;
+        ms = ms < 10 ? "0"+ms : ms;
+
+        if(ms == 100){
+            sec++;
+            sec = sec < 10 ? "0"+sec : sec;
+            ms = 0 + "0";
+        }
+        if(sec == 60){
+
+        }
+        putValue();
+        },10);    
+    }
 
 function resetBtn(){
-    console.log("reset");
+    console.log("reset");   
+    ms = sec = min = 0+"0";
+    putValue();
+    clearInterval(startTimer);
 }
 
 function lapBtn(){
     console.log("lap");
+    const list = document.createElement("li");
+    list.classList.add('link');
+    list.classList.remove('active');
+    list.innerText = "# "+numberOfLap +" : "+ min +" : "+ sec + " : "+ ms ;
+    lapList.appendChild(list);
+    numberOfLap++;
+}
+
+function putValue(){
+    document.querySelector("#ms").innerText = ms;
+    document.querySelector("#sec").innerText = sec;
 }
