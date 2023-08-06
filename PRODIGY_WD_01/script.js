6429ac5cd720fed0f6112bacbf36b86903db6bdb
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-06 13:06:17 +0530

    #

---
+++ PRODIGY_WD_01/script.js
@@ -0,0 +1,19 @@
+var start = document.getElementById("start").addEventListener("click",startBtn);
+var lap = document.getElementById("lap").addEventListener("click",lapBtn);
+var reset = document.getElementById("reset").addEventListener("click",resetBtn);
+
+var min ,sec ,msec;
+min = sec = msec = 0;
+
+function startBtn(){
+    start.
+    console.log("start");
+}
+
+function resetBtn(){
+    console.log("reset");
+}
+
+function lapBtn(){
+    console.log("lap");
+}
