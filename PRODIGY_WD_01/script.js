6902d23b0373b800ef374e64ed54bddef0896eb4
Ayush Sharma <Ayushsharma014may@gmail.com>
2023-08-08 17:07:21 +0530

    added code

--- PRODIGY_WD_01/script.js
+++ PRODIGY_WD_01/script.js
@@ -4,33 +4,43 @@ const lap = document.querySelector(".lap").addEventListener("click",lapBtn);
 const reset = document.querySelector(".reset").addEventListener("click",resetBtn);
 const lapList = document.querySelector(".ol"); 
 
+var startBool = true;
 var min ,sec ,ms, startTimer;
 min = sec = ms = 0+"0" ;
 var numberOfLap = 1;
 
+
+
 function startBtn(){
-    console.log("start");
-    start.classList.add("active");
+    if(startBool){
+        startBool = false;
+        console.log("start");
+        start.classList.add("active");
+        
+        startTimer = setInterval(()=>{
+            ms++;
+            ms = ms < 10 ? "0"+ms : ms;
     
-    startTimer = setInterval(()=>{
-        ms++;
-        ms = ms < 10 ? "0"+ms : ms;
-
-        if(ms == 100){
-            sec++;
-            sec = sec < 10 ? "0"+sec : sec;
-            ms = 0 + "0";
+            if(ms == 100){
+                sec++;
+                sec = sec < 10 ? "0"+sec : sec;
+                ms = 0 + "0";
+            }
+            if(sec == 60){
+    
+            }
+            putValue();
+            },10);    
         }
-        if(sec == 60){
-
-        }
-        putValue();
-        },10);    
     }
+    
 
 function resetBtn(){
-    console.log("reset");   
+    console.log("reset");
+    start.classList.remove("active");
+    startBool = true;
     ms = sec = min = 0+"0";
+    numberOfLap = 0;
     putValue();
     clearInterval(startTimer);
 }
